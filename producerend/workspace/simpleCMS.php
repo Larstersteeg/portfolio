ad54b596a49dcc7408f88013a7fba9d1df6a1227
Lars <460346@edu.rocmn.nl>
2017-03-27 13:18:58 +0200

    producerend leren toegevoegd
    bestanden voor producerend leren toegevoegd aan github

---
+++ producerend/workspace/simpleCMS.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<HTML lang="nl">
+	<head>
+		<title>CMS</title>
+
+	</head>
+	<body>
+	<?php
+	
+	class simpleCMS {
+  var $host;
+  var $username;
+  var $password;
+  var $table;
+
+  public function display_public() {
+    
+  }
+
+  public function display_admin() {
+    
+  }
+
+  public function write() {
+    
+  }
+
+  public function connect() {
+    
+  }
+
+  private function buildDB() {
+    
+  }
+
+}	
+
+mysql_connect("localhost","nabilazzati","") or die(mysql_error());
+
+	?>
+	
+		
+	</body>
+
+</html>
